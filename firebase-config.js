2e1c9f0115317f35094dfb26e069099f6a901d4f
MarsFlow English Academy <110241356+marsflow-englishacademy@users.noreply.github.com>
2026-02-01 12:04:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,13 @@
 // Substitua com suas credenciais do Firebase
 
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "SEU_AUTH_DOMAIN_AQUI",
-    projectId: "SEU_PROJECT_ID_AQUI",
-    storageBucket: "SEU_STORAGE_BUCKET_AQUI",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID_AQUI",
-    appId: "SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyBYe4oRa-uS1hTodVAsa7Zer1fOAU9-kcA",
+  authDomain: "marsflow-english-academy.firebaseapp.com",
+  projectId: "marsflow-english-academy",
+  storageBucket: "marsflow-english-academy.firebasestorage.app",
+  messagingSenderId: "527381031956",
+  appId: "1:527381031956:web:495fb3da0a3cea8b864b01",
+  measurementId: "G-1NC30HNJLQ"
 };
 
 // Instruções para configurar o Firebase:
